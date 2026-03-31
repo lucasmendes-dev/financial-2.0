d8e22122cc29bd4269bce872c3ddd93af538c7c0
Lucas Mendes <lucas.a.mendes97@hotmail.com>
2026-03-31 10:54:21 -0300

    Add missing interface method

--- app/Integrations/BrApiPaidAdapter.php
+++ app/Integrations/BrApiPaidAdapter.php
@@ -17,4 +17,9 @@ class BrApiPaidAdapter implements MarketDataAdapterInterface
     {
         throw new \Exception('Not implemented');
     }
+
+    public function saveFetchedDataToDB(string $assetId, MarketDataDTOInterface $data): void
+    {
+        throw new \Exception('Not implemented');
+    }
 }
